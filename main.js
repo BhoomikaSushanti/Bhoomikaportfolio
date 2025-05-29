6939772a0bfdbc4f67aca09e2ec3bc02663ed547
BhoomikaSushanti <96971943+BhoomikaSushanti@users.noreply.github.com>
2025-05-29 18:54:00 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text", {
+  strings: ["Computer Science Graduate", "Tech Explorer", "Opportunity Seeker"],
+  typeSpeed: 100,
+  backSpeed: 20,
+  backDelay: 1000,
+  loop: true
+});
